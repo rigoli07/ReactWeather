b7c21ec871c9d28876e374bdc87a140e0f31d6f6
Carmelo Rigoli <crigoli@thebrandagency.co>
2017-03-23 14:28:38 +0800

    styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,25 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to examples page</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Perth'>Perth, Aus</Link>                
+                </li>
+                <li>
+                    <Link to='/?location=New York'>New York, NY</Link>              
+                </li>
+                <li>
+                    <Link to='/?location=Boston'>Boston, MA</Link>                
+                </li>
+            </ol>
         </div>
     );    
 }
