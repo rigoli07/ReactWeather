1b042a8b7b6e31b71ac72d892d86c5252e6a0452
Carmelo Rigoli <crigoli@thebrandagency.co>
2017-03-23 16:25:28 +0800

    css styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3 className="text-center">About</h3>
+            <h1 className="text-center page-title">About</h1>
             <p>This is a weather application, built on React.</p>
             <p>Here are some of the tools we used:</p>
             <ul>
